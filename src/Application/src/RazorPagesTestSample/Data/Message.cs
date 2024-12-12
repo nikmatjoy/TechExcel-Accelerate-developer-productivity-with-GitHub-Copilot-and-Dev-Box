580d1f62c048a7f7ec9a1b858f26fa6e66777eef
nikmatjoy <ewheng@hotmail.com>
2024-12-12 20:59:33 +0800

    Resolves #3

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -9,7 +9,20 @@ namespace RazorPagesTestSample.Data
 
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        /// <summary>
+        /// Gets or sets the text message, change max len from 200 to 250.
+        /// </summary>
+        /// <value>
+        /// The text message.
+        /// </value>
+        /// <remarks>
+        /// The text message has a maximum length of 200 characters.
+        /// </remarks>
+        /// <exception cref="System.ComponentModel.DataAnnotations.ValidationException">
+        /// Thrown when the text message exceeds 200 characters.
+        /// </exception>
+        //[StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion
